7944bd6c0c0dbdc127149c678b9d247465ee9190
binaryyoung <asqw4545@gmail.com>
2020-06-23 13:15:55 +0900

    deploy setting

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real 1이 쉬고있음.
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고있고, 반대면 real1이 쉬고있음.
 
+# 유휴 프로파일 탐색
 function find_idle_profile() {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    # 80 포트에 요청을 보낸다
 
-    if [ ${RESPONSE_CODE} -ge 400 ]; # 400보다 크면(즉,40x/50x 에러 모두 포함)
+    if [ ${RESPONSE_CODE} -ge 400 ]
     then
         CURRENT_PROFILE=real2
-    else
+    fi
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == real1 ];
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
         IDLE_PROFILE=real2
     else
@@ -22,11 +24,11 @@ function find_idle_profile() {
     echo "${IDLE_PROFILE}"
 }
 
-# 쉬고 있는 profile의 port 찾기
+# 쉬고 있는 profile의 포트 찾기
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1 ];
+    if [ ${IDLE_PROFILE} == real1 ]
     then
         echo "8081"
     else
